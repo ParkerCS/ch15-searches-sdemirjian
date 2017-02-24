8c06494ff0d52b020a09321916c875f307fe2ea4
sdemirjian <sdemirjian@fwparker.org>
2017-02-24 14:36:14 -0600

    finished function problems

--- ch15Lab.py
+++ ch15Lab.py
@@ -1,6 +1,62 @@
 '''
 Complete the chapter lab at http://programarcadegames.com/index.php?chapter=lab_spell_check
 '''
+import re
 
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
 
+file = open("dictionary.txt")
 
+word_list = []
+
+for line in file:
+    words = split_line(line)
+    for word in words:
+        word_list.append(word)
+
+file.close()
+
+print(" === Linear Search === ")
+file = open("AliceInWonderLand200.txt")
+line_number = 0
+for line in file:
+    line_number += 1
+    words = split_line(line)
+    for word in words:
+        key = word
+        for i in range(len(word_list)):
+            if key.upper() == word_list[i]:
+                break
+        else:
+            print("Possible misspelling at line", line_number,":", key)
+
+file.close()
+
+print(" === Binary Search === ")
+file = open("AliceInWonderLand200.txt")
+line_number = 0
+upper_bound = len(word_list) - 1
+lower_bound = 0
+found = False
+for line in file:
+    line_number += 1
+    words = split_line(line)
+    for word in words:
+        key = word
+        while lower_bound <= upper_bound and not found:
+            middle_position = (lower_bound + upper_bound) // 2
+
+            if word_list[middle_position] < key.upper():
+                lower_bound = middle_position + 1
+            elif word_list[middle_position] > key.upper():
+                upper_bound = middle_position - 1
+            else:
+                found = True
+
+        if not found:
+            print("Possible misspelling at line", line_number, ":", key)
+        found = False
+        upper_bound = len(word_list) - 1
+        lower_bound = 0
+file.close()
--- ch15ProblemSet.py
+++ ch15ProblemSet.py
@@ -6,16 +6,83 @@ from class and from Ch15 of the textbook website
 #1.  (7pts) Write code which finds and prints the longest
 # word in the provided dictionary.  If there are more
 # than one longest word, print them all.
+import re
+def split_line(line):
+    #this function takes in a line of text and returns
+    #a list of words in the line
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
 
+file = open("dictionary.txt")
+
+longest = [" "]
+
+for line in file:
+    words = split_line(line)
+    for word in words:
+        if len(word.upper()) > len(longest[0]):
+            longest = [word]
+        elif len(word) == len(longest[0]):
+            longest.append(word)
+print(longest)
+
+file.close()
 #2.  (10pts)  Write code which finds
 # The total word count AND average word length
 # in "AliceInWonderLand.txt"
+file = open("AliceInWonderLand.txt")
 
+word_count = 0
+average = 0
+
+for line in file:
+    words = split_line(line)
+    for word in words:
+        word_count += 1
+        average += len(word)
+average //= word_count
+
+print("There are", word_count, "words in Alice in Wonderland")
+print("The average word length is:", average)
+
+file.close()
 # CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS
 
-#3 (13pts)  How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
+#3 (13pts)  How many times does "Cheshire" occur in "AliceInWonderLand.txt"?
 # How many times does "Cat" occur?
+cheshire = 0
+cat = 0
+
+file = open("AliceInWonderLand.txt")
+
+for line in file:
+    words = split_line(line)
+    for word in words:
+        if word.upper() == "CHESHIRE":
+            cheshire += 1
+        elif word.upper() == "CAT":
+            cat += 1
+
+print("Cheshire is said", cheshire, "times.")
+print("Cat is said", cat, "times.")
+
+file.close()
 # How many times does "Cheshire" immediately followed by "Cat" occur?
+combo = 0
+previous = ""
+current = ""
+
+file = open("AliceInWonderLand.txt")
+
+for line in file:
+    words = split_line(line)
+    for word in words:
+        current = word
+        if previous.upper() == "CHESHIRE" and current.upper() == "CAT":
+            combo += 1
+        previous = current
+
+print("Cheshire cat is said", combo, "Times")
+file.close()
 
 #### OR #####
 
